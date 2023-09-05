42ddda331846e7d746e89c201dbc863cb33925cf
yamamotohimeka <tribestaff0113@gmail.com>
2023-09-05 16:29:52 +0900

    salary画像差し替え

--- page-recruit.php
+++ page-recruit.php
@@ -16,16 +16,22 @@
         <div class="top-visual">
           <div class="top-visual__bg">
             <img class="pc-topimg" src="<?php echo get_template_directory_uri(); ?>/assets/img/recruit-top.png" alt="">
-            <img class="sp-topimg" src="<?php echo get_template_directory_uri(); ?>/assets/img/recruit-top02.jpg" alt="">
+            <img class="sp-topimg" src="<?php echo get_template_directory_uri(); ?>/assets/img/recruit-top02.jpg"
+              alt="">
           </div>
           <div class="top-visual__text">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/recruit-toptext-royal.png" alt="圧倒的な高収入！安心して長く働ける！頑張る女性を応援します">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/recruit-toptext-royal.png"
+              alt="圧倒的な高収入！安心して長く働ける！頑張る女性を応援します">
           </div>
           <div class="list-bg">
             <ul class="top-visual__list">
-              <li class="top-visual__list--item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/merit01.png" alt="未経験でも大歓迎！安心の研修制度"></li>
-              <li class="top-visual__list--item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/merit02.png" alt="ノルマ一切ナシ！完全自由出勤制度 "></li>
-              <li class="top-visual__list--item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/merit03.png" alt="圧倒的リピート会員数。しっかりと稼げます"></li>
+              <li class="top-visual__list--item"><img
+                  src="<?php echo get_template_directory_uri(); ?>/assets/img/merit01.png" alt="未経験でも大歓迎！安心の研修制度"></li>
+              <li class="top-visual__list--item"><img
+                  src="<?php echo get_template_directory_uri(); ?>/assets/img/merit02.png" alt="ノルマ一切ナシ！完全自由出勤制度 "></li>
+              <li class="top-visual__list--item"><img
+                  src="<?php echo get_template_directory_uri(); ?>/assets/img/merit03.png" alt="圧倒的リピート会員数。しっかりと稼げます">
+              </li>
             </ul>
           </div>
         </div><!-- .top-visual -->
@@ -58,7 +64,8 @@
                 施術以外の業務一切なし！<span class="deep-red">毎日がお給料</span>
               </h3>
               <p class="features__container--medium gothic">
-                経験者の方ならご存知だと思いますが他店舗で入店後にルーム清掃･洗濯･釣り銭の用意･雑費引きがある店舗も存在いたします。当店は施術以外のお仕事はセラピスト退室後<span class="red">スタッフが全て行います</span>。セラピストの方はお客様の施術だけに専念でき、帰宅の際もスムーズに退出いただけます。
+                経験者の方ならご存知だと思いますが他店舗で入店後にルーム清掃･洗濯･釣り銭の用意･雑費引きがある店舗も存在いたします。当店は施術以外のお仕事はセラピスト退室後<span
+                  class="red">スタッフが全て行います</span>。セラピストの方はお客様の施術だけに専念でき、帰宅の際もスムーズに退出いただけます。
               </p>
             </div>
           </div><!-- .features__contaniner -->
@@ -72,46 +79,17 @@
                 <span class="deep-red">プライバシー管理を徹底</span>しているので安心して働けます！
               </h3>
               <p class="features__container--medium gothic">
-                お店のサイトではお顔出しは一切ありません。また、完全個室なので自分の担当以外のお客様や他のセラピストと顔を合わすこともありません。出勤退勤時の出入りも<span class="bold">人目を気にすることなく安心して働いていただけます</span>。
+                お店のサイトではお顔出しは一切ありません。また、完全個室なので自分の担当以外のお客様や他のセラピストと顔を合わすこともありません。出勤退勤時の出入りも<span
+                  class="bold">人目を気にすることなく安心して働いていただけます</span>。
               </p>
             </div>
           </div><!-- .features__contaniner -->
         </section><!-- .features -->
 
         <section class="salary">
-          <div class="salary__container">
-            <h2 class="salary__heading">
-              <div class="salary__heading--skew">お給料待遇</div>
-            </h2>
-            <div class="salary__text">
-              <div class="salary__text--design"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/design01.png" alt=""></div>
-              <div class="text-box">
-                <p class="salary__text--medium left-text">業界TOPクラスの...</p>
-                <p class="salary__text--large"><span class="red-large">60<span class="sixty">％</span>～80<span class="sixty">％</span></span><span class="newline-xs"><br></span>実現！</p>
-                <p class="salary__text--bggold gothic">リラクゼーション界では<span class="newline-sp"><br></span>高バック率を自負しております！</p>
-                <p class="salary__text--bold">90<span class="fortyfour">分</span>8500<span class="fortyfour">円</span>～11000<span class="fortyfour">円</span></p>
-                <p class="salary__text--medium02">本指名料<span class="fiftytwo">2,000</span>円(フルバック)</p>
-                <p class="salary__text--small gothic">※一度施術をした事のあるお客様</p>
-                <p class="salary__text--medium04"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/design02.png" alt="">経験者優遇します<img src="<?php echo get_template_directory_uri(); ?>/assets/img/design02.png" alt=""></p>
-                <p class="salary__text--xl"><span class="red-square">日給保証</span><span class="adjust-height">20,000</span><span class="eightyeight">円</span></p>
-                <p class="salary__text--medium05 gothic">※日給保証には当店の規定がございます。</p>
-                <p class="salary__text--medium05">スキルや指名率により昇給制度有ります</p>
-                <ul class="salary__list">
-                  <li class="salary__list--item">雑費一切なし</li>
-                  <li class="salary__list--item">制服無料貸し出し</li>
-                  <li class="salary__list--item">洗濯業務なし</li>
-                  <li class="salary__list--item">完全日払い制</li>
-                  <li class="salary__list--item">掛け持ちOK</li>
-                  <li class="salary__list--item">完全個室待機</li>
-                  <li class="salary__list--item">交通費全額支給</li>
-                  <li class="salary__list--item">完全自由出勤</li>
-                  <li class="salary__list--item">初心者大歓迎</li>
-                  <li class="salary__list--item">アリバイ対策有り</li>
-                </ul>
-              </div><!-- .text-box -->
-              <div class="salary__text--design rotate"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/design01.png" alt=""></div>
-            </div>
-          </div>
+
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/salary-text.png" alt="">
+
         </section><!-- .salary -->
 
         <section class="simulation">
@@ -125,8 +103,9 @@
                   <img src="<?php echo get_template_directory_uri(); ?>/assets/img/simulation01.png" alt="">
                   <p class="modelbox__img--name gothic">Aさん未経験</p>
                 </div>
-                <p class="modelbox__desc--medium gothic">勤務時間　（12:00~18:00）</p>
-                <p class="modelbox__desc"><span class="bgbrown gothic">1日<span class="thirtyone">の</span>給料</span><span class="modelbox__desc--large">35,000<span class="sixtyfive">円</span></span></p>
+                <p class="modelbox__desc--medium gothic">勤務時間（12:00~18:00）</p>
+                <p class="modelbox__desc"><span class="bgbrown gothic">1日<span class="thirtyone">の</span>給料</span><span
+                    class="modelbox__desc--large">35,000<span class="sixtyfive">円</span></span></p>
                 <table class="modelbox__table">
                   <tbody>
                     <tr>
@@ -170,7 +149,8 @@
                   <p class="modelbox__img--name gothic">Bさん経験者</p>
                 </div>
                 <p class="modelbox__desc--medium gothic">勤務時間　（15:00~26:00）</p>
-                <p class="modelbox__desc"><span class="bgbrown gothic">1日<span class="thirtyone">の</span>給料</span><span class="modelbox__desc--large">65,000<span class="sixtyfive">円</span></span></p>
+                <p class="modelbox__desc"><span class="bgbrown gothic">1日<span class="thirtyone">の</span>給料</span><span
+                    class="modelbox__desc--large">65,000<span class="sixtyfive">円</span></span></p>
                 <table class="modelbox__table">
                   <tbody>
                     <tr>
@@ -245,9 +225,12 @@
                 <li class="worry__list--item"><span class="check"></span>給与システムが明確じゃない…</li>
               </ul>
               <div class="worry__img">
-                <img class="worry__img--mainimg" src="<?php echo get_template_directory_uri(); ?>/assets/img/worry01.png" alt="">
-                <div class="worry__img--deco01"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/moyamoya.png" alt=""></div>
-                <div class="worry__img--deco02"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/konnnahazu.png" alt="こんなはずでは"></div>
+                <img class="worry__img--mainimg"
+                  src="<?php echo get_template_directory_uri(); ?>/assets/img/worry01.png" alt="">
+                <div class="worry__img--deco01"><img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/img/moyamoya.png" alt=""></div>
+                <div class="worry__img--deco02"><img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/img/konnnahazu.png" alt="こんなはずでは"></div>
               </div>
             </div>
           </div><!-- .worry-container -->
@@ -258,10 +241,14 @@
             <p class="safety__heading gothic">ご安心下さい！</p>
             <p class="safety__heading gothic">不誠実な対応・採用は一切致しません！</p>
             <div class="safety__imgbox">
-              <div class="safety__imgbox--img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/safety01.png" alt=""></div>
-              <div class="safety__imgbox--img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/safety02.png" alt=""></div>
+              <div class="safety__imgbox--img"><img
+                  src="<?php echo get_template_directory_uri(); ?>/assets/img/safety01.png" alt=""></div>
+              <div class="safety__imgbox--img"><img
+                  src="<?php echo get_template_directory_uri(); ?>/assets/img/safety02.png" alt=""></div>
             </div>
-            <p class="safety__desc gothic">頑張って働く女性に<span class="fortyfour-red">「稼ぐ実感」</span>と<span class="newline-sp"><br></span><span class="newline-tb"><br></span><span class="fortyfour-red">「働きやすい環境」</span>の両立を徹底しています。</p>
+            <p class="safety__desc gothic">頑張って働く女性に<span class="fortyfour-red">「稼ぐ実感」</span>と<span
+                class="newline-sp"><br></span><span class="newline-tb"><br></span><span
+                class="fortyfour-red">「働きやすい環境」</span>の両立を徹底しています。</p>
           </div><!-- .safety__container -->
         </section><!-- .safety -->
 
@@ -275,11 +262,13 @@
                 <tr>
                   <th class="question__table--q">
                     <div class="question__table--left"><span class="forty">Q</span></div>
-                    <div class="question__table--right"><span class="vertical-adjust">未経験で何も分からないのですが大丈夫でしょうか？</span></div>
+                    <div class="question__table--right"><span class="vertical-adjust">未経験で何も分からないのですが大丈夫でしょうか？</span>
+                    </div>
                   </th>
                   <td class="question__table--a">
                     <div class="question__table--left"><span class="forty">A</span></div>
-                    <div class="question__table--right"><span class="vertical-adjust">もちろん大丈夫です！デビュー前にしっかり親切丁寧に研修しますので未経験の方でも大丈夫です！</span></div>
+                    <div class="question__table--right"><span
+                        class="vertical-adjust">もちろん大丈夫です！デビュー前にしっかり親切丁寧に研修しますので未経験の方でも大丈夫です！</span></div>
                   </td>
                 </tr>
                 <tr>
@@ -289,7 +278,8 @@
                   </th>
                   <td class="question__table--a">
                     <div class="question__table--left"><span class="forty">A</span></div>
-                    <div class="question__table--right"><span class="vertical-adjust">性的サービスは一切ございません！当店は健全なリラクゼーションのお店です。安心して働いていただけます。</span></div>
+                    <div class="question__table--right"><span
+                        class="vertical-adjust">性的サービスは一切ございません！当店は健全なリラクゼーションのお店です。安心して働いていただけます。</span></div>
                   </td>
                 </tr>
                 <tr>
@@ -299,7 +289,8 @@
                   </th>
                   <td class="question__table--a">
                     <div class="question__table--left"><span class="forty">A</span></div>
-                    <div class="question__table--right"><span class="vertical-adjust">ノルマは一切ありません。ノルマを一切気にせず、ご自身のライフスタイルに無理なく合わせて働いていただけます。</span></div>
+                    <div class="question__table--right"><span
+                        class="vertical-adjust">ノルマは一切ありません。ノルマを一切気にせず、ご自身のライフスタイルに無理なく合わせて働いていただけます。</span></div>
                   </td>
                 </tr>
                 <tr>
@@ -309,7 +300,8 @@
                   </th>
                   <td class="question__table--a">
                     <div class="question__table--left"><span class="forty">A</span></div>
-                    <div class="question__table--right"><span class="vertical-adjust">30代～50代の会社員・経営者など、マナーのある紳士的な男性を客層としています。安心して働いていただけます。</span></div>
+                    <div class="question__table--right"><span
+                        class="vertical-adjust">30代～50代の会社員・経営者など、マナーのある紳士的な男性を客層としています。安心して働いていただけます。</span></div>
                   </td>
                 </tr>
                 <tr>
@@ -319,7 +311,9 @@
                   </th>
                   <td class="question__table--a">
                     <div class="question__table--left"><span class="forty">A</span></div>
-                    <div class="question__table--right"><span class="vertical-adjust">副業・ダブルワーク大丈夫です！キャリアを積めば独立開業も夢ではありあません。そのためのキャリアアップサポートもしっかりいたします。</span></div>
+                    <div class="question__table--right"><span
+                        class="vertical-adjust">副業・ダブルワーク大丈夫です！キャリアを積めば独立開業も夢ではありあません。そのためのキャリアアップサポートもしっかりいたします。</span>
+                    </div>
                   </td>
                 </tr>
                 <tr>
@@ -329,7 +323,8 @@
                   </th>
                   <td class="question__table--a heightauto">
                     <div class="question__table--left"><span class="forty-adjust">A</span></div>
-                    <div class="question__table--right"><span class="vertical-adjust">もちろん大丈夫です！不安がないように面接で包み隠さずお話をさせていただきます。その場でスグに決めなくても、じっくり考えていただいてからで構いませんのでご検討ください。</span>
+                    <div class="question__table--right"><span
+                        class="vertical-adjust">もちろん大丈夫です！不安がないように面接で包み隠さずお話をさせていただきます。その場でスグに決めなくても、じっくり考えていただいてからで構いませんのでご検討ください。</span>
                       <div>
                   </td>
                 </tr>
@@ -345,11 +340,13 @@
             </h2>
             <p class="recruit-contact__info gothic">TEL：<span class="thirtyfive">080-9299-4519</span></p>
             <!-- <p class="recruit-contact__info gothic">MAIL：sk-p@abcdefg.com</p> -->
-            <div class="recruit-line"><a href="https://line.me/ti/p/6GV7QoHXlq" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/recruit-line.png" alt=""></a></div>
+            <div class="recruit-line"><a href="https://line.me/ti/p/6GV7QoHXlq" target="_blank"><img
+                  src="<?php echo get_template_directory_uri(); ?>/assets/img/recruit-line.png" alt=""></a></div>
             <p class="recruit-contact__desc gothic">電話・ショートメール・LINEでお気軽にご質問・ご応募ください。</p>
             <div class="recruit-contact__qr">
               <div class="qr-flex">
-                <div class="qr-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/recruit-qr.png" alt=""></div>
+                <div class="qr-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/recruit-qr.png"
+                    alt=""></div>
                 <p class="recruit-contact__qr--medium gothic">
                   QRコード読み取りでスグにお友達追加ができます。
                 </p>
